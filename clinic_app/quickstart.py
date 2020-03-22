0b62db016c451cf122a7aa214506e0194e2edab5
nilaybhatia <bhatianilay@gmail.com>
2020-03-22 19:47:02 +0530

    Basic appointment add working from script

--- clinic_app/quickstart.py
+++ clinic_app/quickstart.py
@@ -10,7 +10,7 @@ from google.auth.transport.requests import Request
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 
-def get_current_calendar():
+def main():
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
@@ -27,7 +27,7 @@ def get_current_calendar():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'clinic_app/credentials.json', SCOPES)
+                'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
@@ -43,79 +43,43 @@ def get_current_calendar():
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
 
-    current_calendar = []
     if not events:
         print('No upcoming events found.')
     for event in events:
-        print(event)
-        only_releavant_fields = {}
-        only_releavant_fields['summary'] = event['summary']
-        only_releavant_fields['start'] = event['start']
-        only_releavant_fields['end'] = event['end']
-        current_calendar.append(only_releavant_fields)
-        #start = event['start'].get('dateTime', event['start'].get('date'))
-        #print(start, event['summary'])
-    
-    return current_calendar
-
-def add_appointment_to_calendar():
-    # Refer to the Python quickstart on how to setup the environment:
-    # https://developers.google.com/calendar/quickstart/python
-    # Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
-    # stored credentials.
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
 
     event = {
         'summary': 'Google I/O 2015',
         'location': '800 Howard St., San Francisco, CA 94103',
         'description': 'A chance to hear more about Google\'s developer products.',
         'start': {
-            'dateTime': '2015-05-28T09:00:00-07:00',
-            'timeZone': 'America/Los_Angeles',
+        'dateTime': '2015-05-28T09:00:00-07:00',
+        'timeZone': 'America/Los_Angeles',
         },
         'end': {
-            'dateTime': '2015-05-28T17:00:00-07:00',
-            'timeZone': 'America/Los_Angeles',
+        'dateTime': '2015-05-28T17:00:00-07:00',
+        'timeZone': 'America/Los_Angeles',
         },
         'recurrence': [
-            'RRULE:FREQ=DAILY;COUNT=2'
+        'RRULE:FREQ=DAILY;COUNT=2'
         ],
         'attendees': [
             {'email': 'lpage@example.com'},
             {'email': 'sbrin@example.com'},
         ],
         'reminders': {
-            'useDefault': False,
-            'overrides': [
+        'useDefault': False,
+        'overrides': [
             {'method': 'email', 'minutes': 24 * 60},
             {'method': 'popup', 'minutes': 10},
-            ],
+        ],
         },
     }
-
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'clinic_app/credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('calendar', 'v3', credentials=creds)
 
     event = service.events().insert(calendarId='primary', body=event).execute()
     print ('Event created: %s' % (event.get('htmlLink')))
 
 
 if __name__ == '__main__':
-    get_current_calendar()
+    main()

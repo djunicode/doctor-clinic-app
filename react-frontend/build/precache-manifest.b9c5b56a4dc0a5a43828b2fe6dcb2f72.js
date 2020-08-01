c632fabd98efa1f9f4e5a5b01a2586f3331d2dd0
Rishi N Desai <rishindesai@yahoo.com>
2020-08-01 20:54:43 +0530

    build

---
+++ react-frontend/build/precache-manifest.b9c5b56a4dc0a5a43828b2fe6dcb2f72.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a07b20ffe8984d68085e140e7926f891",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b308efb6e74c4a1d8f63",
+    "url": "/static/css/2.6d29bf6f.chunk.css"
+  },
+  {
+    "revision": "ce55ac57409e9036473b",
+    "url": "/static/css/main.cd4a40d4.chunk.css"
+  },
+  {
+    "revision": "b308efb6e74c4a1d8f63",
+    "url": "/static/js/2.dca123e6.chunk.js"
+  },
+  {
+    "revision": "bca5fadb535527aa41fda2ee0a3ee7cf",
+    "url": "/static/js/2.dca123e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce55ac57409e9036473b",
+    "url": "/static/js/main.743b8bd9.chunk.js"
+  },
+  {
+    "revision": "18b37c663b0eb8c87f7c",
+    "url": "/static/js/runtime-main.9e863732.js"
+  },
+  {
+    "revision": "7ca9ab5f8d63a22b731cc88666199816",
+    "url": "/static/media/defaultdp.7ca9ab5f.webp"
+  },
+  {
+    "revision": "b4fb5e213005a84dee22d7fa01869a98",
+    "url": "/static/media/hospital.b4fb5e21.jpg"
+  }
+]);
